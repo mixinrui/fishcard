a80c66ccb1dedbbe051b579ae2ec8a7a73df8562
hucl <hucl@hucldeiMac.local>
2016-07-08 21:20:57 +0800

    向小马发送消息

---
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/common/bean/TeachingOnlineMsg.java
@@ -0,0 +1,12 @@
+package com.boxfishedu.workorder.common.bean;
+
+import lombok.Data;
+
+/**
+ * Created by hucl on 16/7/8.
+ */
+@Data
+public class TeachingOnlineMsg {
+    private Long user_id;
+    private String push_title;
+}
--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/requester/CourseOnlineRequester.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/requester/CourseOnlineRequester.java
@@ -1,6 +1,7 @@
 package com.boxfishedu.workorder.requester;
 
 import com.boxfishedu.workorder.common.bean.TeachingNotificationEnum;
+import com.boxfishedu.workorder.common.bean.TeachingOnlineMsg;
 import com.boxfishedu.workorder.common.config.UrlConf;
 import com.boxfishedu.workorder.common.threadpool.ThreadPoolManager;
 import com.boxfishedu.workorder.entity.mysql.WorkOrder;
@@ -37,5 +38,16 @@ public class CourseOnlineRequester {
                 urlConf.getCourse_online_service(),workOrderId,TeachingNotificationEnum.toString());
         logger.debug("<<<<<<<<<<<<<@[notifyTeachingOnlinePushMessage]向在线教育发起通知操作,[[[[通知在线教学中心推送消息]]]],url[{}]",url);
         threadPoolManager.execute(new Thread(()->{restTemplate.getForObject(url,Object.class);}));
+    }
+
+    /**
+     * 发送封装好的消息
+     */
+    public void pushWrappedMsg(TeachingOnlineMsg teachingOnlineMsg){
+        String url=String.format("%s/teaching/callback/push",
+                urlConf.getCourse_online_service());
+        logger.debug("<<<<<<<<<<<<<@[pushWrappedMsg]向在线教育发起通知操作,[[[[通知用户[{}]推送消息[{}]]]]],url[{}]",url,teachingOnlineMsg.getUser_id(),teachingOnlineMsg.getPush_title());
+        threadPoolManager.execute(new Thread(()->{restTemplate.postForObject(url,teachingOnlineMsg,Object.class);}));
+
     }
 }
