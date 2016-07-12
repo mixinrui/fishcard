834a1513075f30ea8ecbb0d7eda911c3e1da5cb9
mk <331915933@qq.com>
2016-07-12 13:58:49 +0800

    add graborderview

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/controller/graborder/GrabOrderController.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/controller/graborder/GrabOrderController.java
@@ -1,20 +1,20 @@
 package com.boxfishedu.workorder.web.controller.graborder;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.boxfishedu.workorder.common.util.DateUtil;
 import com.boxfishedu.workorder.common.util.WorkOrderConstant;
 import com.boxfishedu.workorder.entity.mysql.WorkOrderGrab;
 import com.boxfishedu.workorder.web.view.base.JsonResultModel;
+import com.boxfishedu.workorder.web.view.fishcard.GrabOrderView;
 import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 /**
- * 此接口主要提供给鱼卡中心的管理后台使用,主要包括:鱼卡列表,换课,换教师,换时间
+ * 抢单
  */
 @CrossOrigin
 @RestController
@@ -28,31 +28,33 @@ public class GrabOrderController {
     public JsonResultModel getWorkOrderListByTeacherId(@PathVariable("teacher_id") Long teacherId) {
 
         List<WorkOrderGrab> fishCardList = Lists.newArrayList();
+        JSONArray jsonArray = new JSONArray();
 
         Long workOrderId1 = 111111111L;
         Long workOrderId2 = 222222222L;
 
         WorkOrderGrab workOrderGrabMock1 = new WorkOrderGrab();
-        workOrderGrabMock1.setWorkorderId(workOrderId1);
-        workOrderGrabMock1.setStartTime(DateUtil.date2SimpleDate(new Date()));
-        fishCardList.add(workOrderGrabMock1);
+        JSONObject jsonObject1 = new JSONObject();
+        jsonObject1.put("workorderId",workOrderId1);
+        jsonObject1.put("startTime","2016-06-13 21:00:00");
+        jsonArray.add(jsonObject1);
 
-        WorkOrderGrab workOrderGrabMock2 = new WorkOrderGrab();
-        workOrderGrabMock2.setWorkorderId(workOrderId2);
-        workOrderGrabMock2.setStartTime(DateUtil.date2SimpleDate(new Date()));
-        fishCardList.add(workOrderGrabMock2);
+        JSONObject jsonObject2 = new JSONObject();
+        jsonObject2.put("workorderId",workOrderId2);
+        jsonObject2.put("startTime","2016-06-13 21:30:00");
+        jsonArray.add(jsonObject2);
 
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("msg", WorkOrderConstant.FISHCARD_LIST);
         jsonObject.put("code",0);
-        jsonObject.put("workorderlist",fishCardList);
+        jsonObject.put("workorderlist",jsonArray);
 
         return JsonResultModel.newJsonResultModel(jsonObject);
     }
 
 
     @RequestMapping(value = "/graboneorder", method = RequestMethod.POST)
-    public JsonResultModel grabOrder() {
+    public JsonResultModel grabOrder(@RequestBody GrabOrderView grabOrderView) {
         int i = (int)(Math.random()*2);
         JSONObject jsonObject = new JSONObject();
         if(i==0){
@@ -64,6 +66,7 @@ public class GrabOrderController {
         }
         return JsonResultModel.newJsonResultModel(jsonObject);
     }
+
 
 
 
---
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/view/fishcard/GrabOrderView.java
@@ -0,0 +1,15 @@
+package com.boxfishedu.workorder.web.view.fishcard;
+
+import lombok.Data;
+
+/**
+ * Created by mk on 16/7/12.
+ */
+@Data
+public class GrabOrderView {
+
+    private Long teacherId;
+
+    private Long fishcardId;
+
+}
