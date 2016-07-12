616d1887d6fd2458ec475c26c719d81e32c991f0
mk <331915933@qq.com>
2016-07-12 18:15:42 +0800

    commit grab method

---
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/service/graborder/GrabOrderService.java
@@ -0,0 +1,7 @@
+package com.boxfishedu.workorder.service.graborder;
+
+/**
+ * Created by mk on 16/7/12.
+ */
+public class GrabOrderService {
+}
---
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/graborder/GrabOrderServiceX.java
@@ -0,0 +1,7 @@
+package com.boxfishedu.workorder.servicex.graborder;
+
+/**
+ * Created by mk on 16/7/12.
+ */
+public class GrabOrderServiceX {
+}
