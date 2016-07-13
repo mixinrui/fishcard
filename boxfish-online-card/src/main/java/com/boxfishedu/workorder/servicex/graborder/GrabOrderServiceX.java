eeff8f2caf9b31d3c678a1e2bd7efa5e3f1323b8
mk <331915933@qq.com>
2016-07-13 12:11:04 +0800

    add grab order

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/graborder/GrabOrderServiceX.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/graborder/GrabOrderServiceX.java
@@ -1,11 +1,9 @@
 package com.boxfishedu.workorder.servicex.graborder;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.boxfishedu.workorder.common.config.UrlConf;
 import com.boxfishedu.workorder.common.exception.BoxfishException;
 import com.boxfishedu.workorder.common.redis.CacheKeyConstant;
-import com.boxfishedu.workorder.common.util.JacksonUtil;
 import com.boxfishedu.workorder.common.util.WorkOrderConstant;
 import com.boxfishedu.workorder.entity.mysql.WorkOrder;
 import com.boxfishedu.workorder.entity.mysql.WorkOrderGrab;
@@ -20,13 +18,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.*;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by mk on 16/7/12.
  */
+@Component
 public class GrabOrderServiceX {
 
     //本地异常日志记录对象
@@ -50,18 +53,19 @@ public class GrabOrderServiceX {
 
     public JsonResultModel getWorkOrderListByTeacherId(Long teacherId){
         List<WorkOrderGrab> listWorkOrderGrab = this.getFromMySql(teacherId);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("msg", WorkOrderConstant.FISHCARD_LIST);
         if(listWorkOrderGrab!=null&&listWorkOrderGrab.size()>0){
-            logger.info("::::::::::::::::::根据teacherId从Redis中获取可抢课程列表::::::::::::::::::");
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("msg", WorkOrderConstant.FISHCARD_LIST);
+            logger.error("::::::::::::::::::获取该老师可以抢的课程列表::::::::::::::::::");
             jsonObject.put("code",0);
             jsonObject.put("workorderlist",listWorkOrderGrab);
-            return JsonResultModel.newJsonResultModel(jsonObject);
         }
         else{
-            logger.error("::::::::::::::::::没有需要补老师的课程-缓存与数据库中都没有可抢课程::::::::::::::::::");
-            return JsonResultModel.newJsonResultModel(null);
+            logger.error("::::::::::::::::::数据库中都没有可抢课程::::::::::::::::::");
+            jsonObject.put("code",1);
+            jsonObject.put("workorderlist",null);
         }
+        return JsonResultModel.newJsonResultModel(jsonObject);
     }
 
     //TODO 等待被召唤的一天
@@ -105,26 +109,48 @@ public class GrabOrderServiceX {
 
     public JsonResultModel grabOrderByOneTeacher(GrabOrderView grabOrderView){
         JSONObject jsonObject = new JSONObject();
-        if(checkIfCanGrabOrder(grabOrderView)){
+        if(!checkIfCanGrabOrderByOnlineTeacher(grabOrderView)||!checkIfCanGrabOrderByOnlineFishcard(grabOrderView)){
             jsonObject.put("msg",WorkOrderConstant.GRABORDER_FAIL);
             jsonObject.put("code","0");
         }else{
-            grabOrderService.setFlagAndTeacherId(grabOrderView);
-            grabOrderService.setTeacherIdByWorkOrderId(grabOrderView);
+            grabOrderService.setFlagSuccessAndTeacherId(grabOrderView);
+            WorkOrder workOrder = grabOrderService.findByIdForUpdate(grabOrderView.getWorkOrderId());
+            if(workOrder!=null){
+                grabOrderService.setTeacherIdByWorkOrderId(grabOrderView);
+            }
             jsonObject.put("msg",WorkOrderConstant.GRABORDER_SUCCESS);
             jsonObject.put("code","1");
+            logger.info("::::::::::::::::::成功抢单::::::::::::::::::");
         }
         return JsonResultModel.newJsonResultModel(jsonObject);
     }
 
-    private boolean checkIfCanGrabOrder(GrabOrderView grabOrderView) throws BoxfishException {
+    private boolean checkIfCanGrabOrderByOnlineFishcard(GrabOrderView grabOrderView){
+        WorkOrder workOrder = workOrderService.findOne(grabOrderView.getWorkOrderId());
+        if(workOrder!=null){
+            if(workOrder.getTeacherId()!=null&&workOrder.getTeacherId()>0){    //该课程已经被其他老师抢了
+                logger.info("::::::::::::::::::OnlineFishcard验证----不能抢::::::::::::::::::");
+                grabOrderService.setFlagFailAndTeacherId(grabOrderView);
+                return false;
+            }else{
+                logger.info("::::::::::::::::::OnlineFishcard验证----能抢::::::::::::::::::");
+                return true;
+            }
+        }
+        logger.info("::::::::::::::::::OnlineFishcard验证----不能抢(在workorder表中根据workOrderId没有查出记录)::::::::::::::::::");
+        return false;
+    }
+
+    private boolean checkIfCanGrabOrderByOnlineTeacher(GrabOrderView grabOrderView) throws BoxfishException {
         Map<String,Object> mapParams = this.makeParams(grabOrderView);
-        String url = "192.168.77.210:8099/order/course/schedule/add/order/time";   //TODO
+        String url = "http://192.168.77.210:8099/order/course/schedule/add/order/time";   //TODO
         JsonResultModel jsonResultModel=restTemplate.postForObject(url,mapParams,JsonResultModel.class);
         if(jsonResultModel.getReturnCode()== HttpStatus.OK.value()){
-            logger.error("::::::::::::::::::teacherId:"+grabOrderView.getTeacherId()+"可以抢workorderId:"+grabOrderView.getWorkOrderId()+"的课程::::::::::::::::::");
+            logger.info("::::::::::::::::::OnlineTeacher验证----能抢::::::::::::::::::");
             return true;
         }else{
+            logger.info("::::::::::::::::::OnlineTeacher验证----不能抢::::::::::::::::::");
+            grabOrderService.setFlagFailAndTeacherId(grabOrderView);
             return false;
         }
     }
