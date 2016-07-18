e8d51fc3316fb0f454dd4e741107236fc1d95a5a
hucl <hucl@hucldeiMac.local>
2016-07-18 14:04:29 +0800

    增加新表cfommentcard

---
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/dao/jpa/CommentCardJpaRepository.java
@@ -0,0 +1,8 @@
+package com.boxfishedu.workorder.dao.jpa;
+
+import com.boxfishedu.workorder.entity.mysql.CommentCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentCardJpaRepository extends JpaRepository<CommentCard, Long> {
+
+}
