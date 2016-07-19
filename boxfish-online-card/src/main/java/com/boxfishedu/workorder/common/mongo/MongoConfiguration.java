a15fd03f3fb37188444f79bfe4d182286e4e75a0
hucl <hucl@hucldeiMac.local>
2016-07-19 22:08:27 +0800

    提交外教点评卡的日志

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/common/mongo/MongoConfiguration.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/common/mongo/MongoConfiguration.java
@@ -3,6 +3,7 @@ package com.boxfishedu.workorder.common.mongo;
 import com.boxfishedu.workorder.entity.mongo.ScheduleCourseInfo;
 import com.boxfishedu.workorder.entity.mongo.TrialCourse;
 import com.boxfishedu.workorder.entity.mongo.WorkOrderLog;
+import com.boxfishedu.workorder.entity.mysql.CommentCard;
 import com.mongodb.*;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
@@ -71,6 +72,7 @@ public class MongoConfiguration {
     @Bean
     Datastore datastore() {
         Morphia morphia = morphia();
+        morphia.map(CommentCard.class);
         morphia.map(WorkOrderLog.class);
         morphia.map(ScheduleCourseInfo.class);
         morphia.map(TrialCourse.class);
--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/dao/mongo/CommentCardLogMorphiaRepository.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/dao/mongo/CommentCardLogMorphiaRepository.java
@@ -1,37 +1,18 @@
 package com.boxfishedu.workorder.dao.mongo;
 
 import com.boxfishedu.workorder.entity.mongo.CommentCardLog;
-import com.boxfishedu.workorder.entity.mongo.WorkOrderLog;
-import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.query.Query;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Optional;
 
 @Component
 public class CommentCardLogMorphiaRepository extends BaseMorphiaRepository<CommentCardLog> {
-
-    public Optional<WorkOrderLog> getById(Long id) {
-        Query<WorkOrderLog> query = datastore.createQuery(WorkOrderLog.class);
-        query.criteria("id").equal(id);
-        query.limit(1);
-        return Optional.ofNullable(query.get());
-    }
-
-    public List<WorkOrderLog> queryByCommentCardId(Long workId) {
-        Query<WorkOrderLog> query = datastore.createQuery(WorkOrderLog.class);
-        query.and(query.criteria("commentCardId").equal(workId));
-        //倒序
-//        query.order("-createTime");
+    public List<CommentCardLog> queryByCommentCardId(Long commentCardId) {
+        Query<CommentCardLog> query = datastore.createQuery(CommentCardLog.class);
+        query.and(query.criteria("commentCardId").equal(commentCardId));
         query.order("createTime");
-        List<WorkOrderLog> workOrderLogs= query.asList();
-        int limit=0;
-        for (WorkOrderLog workOrderLog:workOrderLogs){
-            limit++;
-            workOrderLog.setSeqNum(limit);
-        }
-        return workOrderLogs;
+        List<CommentCardLog> commentCardLogs = query.asList();
+        return commentCardLogs;
     }
 }
