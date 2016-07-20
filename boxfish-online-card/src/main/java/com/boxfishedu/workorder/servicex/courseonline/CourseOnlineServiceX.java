a6eb26a3888b95e2e7d2a46001ad5873d514cc8d
hucl <hucl@hucldeiMac.local>
2016-07-20 14:18:05 +0800

    增加终端上报时间

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/courseonline/CourseOnlineServiceX.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/courseonline/CourseOnlineServiceX.java
@@ -82,7 +82,7 @@ public class CourseOnlineServiceX {
         WorkOrder workOrder = workOrderService.findOne(workOrderId);
         CourseSchedule courseSchedule= courseScheduleService.findByWorkOrderId(workOrderId);
         if((status!=FishCardStatusEnum.COMPLETED.getCode())&&(status!=FishCardStatusEnum.COMPLETED_FORCE.getCode())) {
-            courseOnlineService.notAllowUpdateStatus(workOrder,status);
+            courseOnlineService.notAllowUpdateStatus(workOrder,"不能覆盖已有消息,新消息:"+FishCardStatusEnum.getDesc(status)+"@@reportTime"+reportTime);
         }
         if (null == workOrder||null==courseSchedule) {
             String msg="无对应的鱼卡或课程schedule,请确认参数传入是否正确,鱼卡id[" + workOrderId + "]";
