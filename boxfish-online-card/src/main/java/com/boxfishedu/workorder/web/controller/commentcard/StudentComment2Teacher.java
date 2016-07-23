e55565150390f89c980395b2d94d4953591a6d4c
hucl <hucl@hucldeiMac.local>
2016-07-23 18:30:56 +0800

    提交状态修改

---
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/controller/commentcard/StudentComment2Teacher.java
@@ -0,0 +1,20 @@
+package com.boxfishedu.workorder.web.controller.commentcard;
+
+import com.boxfishedu.workorder.web.param.Student2TeacherCommentParam;
+import com.boxfishedu.workorder.web.view.base.JsonResultModel;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by hucl on 16/7/23.
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/comment/foreign")
+
+public class StudentComment2Teacher {
+    @RequestMapping(value = "/student/comment", method = RequestMethod.POST)
+    public JsonResultModel studentComment2Teacher(@RequestBody Student2TeacherCommentParam student2TeacherCommentParam) {
+
+        return null;
+    }
+}
