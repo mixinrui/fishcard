3eb15ac0f816dd613292bdc188459de620b11db6
hucl <hucl@hucldeiMac.local>
2016-07-25 21:12:48 +0800

    提醒海江接口增加评价

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/requester/TeacherStudentCommentCardRequester.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/requester/TeacherStudentCommentCardRequester.java
@@ -9,11 +9,16 @@ import com.boxfishedu.workorder.common.exception.BusinessException;
 import com.boxfishedu.workorder.common.threadpool.ThreadPoolManager;
 import com.boxfishedu.workorder.common.util.DateUtil;
 import com.boxfishedu.workorder.common.util.JacksonUtil;
+import com.boxfishedu.workorder.entity.mysql.CommentCard;
 import com.boxfishedu.workorder.entity.mysql.CourseSchedule;
 import com.boxfishedu.workorder.entity.mysql.WorkOrder;
+import com.boxfishedu.workorder.requester.param.ForeignTeacherSetCommentParam;
+import com.boxfishedu.workorder.service.commentcard.CommentCardTeacherAppService;
+import com.boxfishedu.workorder.service.commentcard.ForeignTeacherCommentCardService;
 import com.boxfishedu.workorder.servicex.bean.DayTimeSlots;
 import com.boxfishedu.workorder.servicex.bean.TimeSlots;
 import com.boxfishedu.workorder.web.param.FetchTeacherParam;
+import com.boxfishedu.workorder.web.param.Student2TeacherCommentParam;
 import com.boxfishedu.workorder.web.view.base.JsonResultModel;
 import com.boxfishedu.workorder.web.view.teacher.PlannerAssignView;
 import com.boxfishedu.workorder.web.view.teacher.TeacherView;
@@ -40,17 +45,31 @@ public class TeacherStudentCommentCardRequester {
     private UrlConf urlConf;
     @Autowired
     private RestTemplate restTemplate;
-    @Autowired
-    private ThreadPoolManager threadPoolManager;
 
     @Autowired
     private CacheManager cacheManager;
 
-    private Logger logger= LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private ThreadPoolManager threadPoolManager;
+
+    @Autowired
+    private CommentCardTeacherAppService commentCardTeacherAppService;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
-    public void sendStudentComment2Teaxcher(){
-
+    public void sendStudentComment2Teacher(Student2TeacherCommentParam student2TeacherCommentParam) {
+        CommentCard commentCard=commentCardTeacherAppService.findById(student2TeacherCommentParam.getCommentCardId());
+        if(null==commentCard){
+            throw new BusinessException("不存在对应的点评卡:["+student2TeacherCommentParam.getCommentCardId()+"]");
+        }
+        ForeignTeacherSetCommentParam foreignTeacherSetCommentParam=ForeignTeacherSetCommentParam.paramAdapter(student2TeacherCommentParam,commentCard);
+        String url = String.format("%s/f_teacher_review/set_review", urlConf.getTeacher_service());
+        logger.debug("<<<<<<<<<<<<<@[sendStudentComment2Teaxcher]向师生运营发起[[[[设置鱼卡的点评信息]]]],url[{}]", url);
+        threadPoolManager.execute(new Thread(() -> {
+            restTemplate.postForObject(url, foreignTeacherSetCommentParam, JsonResultModel.class);
+        }));
     }
+
 
 }
