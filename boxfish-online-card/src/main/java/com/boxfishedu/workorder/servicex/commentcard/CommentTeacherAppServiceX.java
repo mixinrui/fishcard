60a0d36f27865b0032a8a6f2264ade9e8b83786a
hucl <hucl@hucldeiMac.local>
2016-07-28 13:02:26 +0800

    教师提交评价以后修改相关参数

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/commentcard/CommentTeacherAppServiceX.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/commentcard/CommentTeacherAppServiceX.java
@@ -5,6 +5,7 @@ import com.boxfishedu.workorder.common.exception.BusinessException;
 import com.boxfishedu.workorder.entity.mysql.CommentCard;
 import com.boxfishedu.workorder.requester.TeacherStudentCommentCardRequester;
 import com.boxfishedu.workorder.requester.TeacherStudentRequester;
+import com.boxfishedu.workorder.service.commentcard.CommentCardLogService;
 import com.boxfishedu.workorder.service.commentcard.CommentCardTeacherAppService;
 import com.boxfishedu.workorder.web.param.CommentCardSubmitParam;
 import com.boxfishedu.workorder.web.param.Student2TeacherCommentParam;
@@ -27,6 +28,9 @@ public class CommentTeacherAppServiceX {
 
     @Autowired
     private CommentCardTeacherAppService commentCardTeacherAppService;
+
+    @Autowired
+    private CommentCardLogService commentCardLogService;
 
     @Autowired
     private TeacherStudentCommentCardRequester teacherStudentCommentCardRequester;
@@ -56,10 +60,12 @@ public class CommentTeacherAppServiceX {
         if(null==commentCard){
             throw new BusinessException("不存在对应的点评卡");
         }
+        commentCard.setStudentReadFlag(0);
         commentCard.setStatus(CommentCardStatus.ANSWERED.getCode());
         commentCard.setUpdateTime(new Date());
         commentCard.setAnswerVideoPath(commentCardSubmitParam.getVideoPath());
         commentCard.setTeacherAnswerTime(new Date());
         commentCardTeacherAppService.save(commentCard);
+        commentCardLogService.saveCommentCardLog(commentCard);
     }
 }
