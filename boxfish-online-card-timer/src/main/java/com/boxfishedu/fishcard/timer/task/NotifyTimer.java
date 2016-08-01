828646b5b2ed8818642572abe3697f54b782f1cf
jiaozijun <java_jzj0828@163.com>
2016-08-01 10:07:50 +0800

    完善正式环境配置

--- boxfish-online-card-timer/src/main/java/com/boxfishedu/fishcard/timer/task/NotifyTimer.java
+++ boxfish-online-card-timer/src/main/java/com/boxfishedu/fishcard/timer/task/NotifyTimer.java
@@ -123,8 +123,8 @@ public class NotifyTimer {
     /**
      * 抢单: 每天18点到 24点  每10分钟 轮训查询
      */
-//   @Scheduled(cron = "0 0/10 18,19,20,21,22,23 * * ?")
-    @Scheduled(cron = "0 0/5 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * ?")
+    @Scheduled(cron = "0 0/10 18,19,20,21,22,23 * * ?")
+   //   @Scheduled(cron = "0 0/5 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * ?")
     public void initGrabOrderDataChinese() {
         logger.info("<<<<<<graborder-initGrabOrderData<<<<<<<<<<<<<<<<");
         logger.info("<<<<<<开始通知<<<轮训抢单初始化数据>>>的消息,时间[{}]", DateUtil.Date2String(new Date()));
@@ -147,8 +147,8 @@ public class NotifyTimer {
     }
 
 
-    //   @Scheduled(cron = "0 0/10 18,19,20,21,22,23 * * ?")
-   @Scheduled(cron = "0 0/5 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * ?")
+    @Scheduled(cron = "0 0/10 18,19,20,21,22,23 * * ?")
+    //@Scheduled(cron = "0 0/5 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * ?")
     public void initGrabOrderDataForeigh() {
         logger.info("<<<<<<graborder-initGrabOrderDataForeigh<<<<<<<<<<<<<<<<");
         logger.info("<<<<<<开始通知<<<轮训抢单初始化数据外教>>>的消息,时间[{}]", DateUtil.Date2String(new Date()));
