3834eeea650ec5612a705c1effe70e63fa8a2a92
hucl <hucl@hucldeiMac.local>
2016-08-14 18:51:55 +0800

    整理代码

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/controller/teacherrelated/CommentCourseController.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/controller/teacherrelated/CommentCourseController.java
@@ -3,10 +3,10 @@ package com.boxfishedu.workorder.web.controller.teacherrelated;
 import com.boxfishedu.workorder.servicex.studentrelated.CommentCourseInfoServiceX;
 import com.boxfishedu.workorder.web.view.base.JsonResultModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by hucl on 16/8/14.
