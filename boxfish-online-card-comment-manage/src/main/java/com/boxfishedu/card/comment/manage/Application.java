03f93ea802f7e28b1e4840aa9ca908366c16afb6
luolibing <397911353@qq.com>
2016-09-02 15:43:45 +0800

    外教点评后台

---
+++ boxfish-online-card-comment-manage/src/main/java/com/boxfishedu/card/comment/manage/Application.java
@@ -0,0 +1,15 @@
+package com.boxfishedu.card.comment.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by LuoLiBing on 16/9/2.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
