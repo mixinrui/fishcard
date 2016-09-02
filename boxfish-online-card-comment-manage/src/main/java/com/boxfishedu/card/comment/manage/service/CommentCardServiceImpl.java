299c13107d4de92670dca7418c46665dec5dcad3
luolibing <397911353@qq.com>
2016-09-02 16:11:13 +0800

    外教点评包

---
+++ boxfish-online-card-comment-manage/src/main/java/com/boxfishedu/card/comment/manage/service/CommentCardServiceImpl.java
@@ -0,0 +1,10 @@
+package com.boxfishedu.card.comment.manage.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by LuoLiBing on 16/9/2.
+ */
+@Service
+public class CommentCardServiceImpl {
+}
