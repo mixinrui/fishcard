66b5f9f63cee1405eba9d8f68b8226e9613acdff
ansel <liwanxiang@boxfish.cn>
2016-09-02 16:46:24 +0800

    新增外教点评后台

---
+++ boxfish-online-card-comment-manage/src/main/java/com/boxfishedu/card/comment/manage/entity/mysql/ToTeacherStudentForm.java
@@ -0,0 +1,10 @@
+package com.boxfishedu.card.comment.manage.entity.mysql;
+
+import lombok.Data;
+
+/**
+ * Created by ansel on 16/9/2.
+ */
+@Data
+public class ToTeacherStudentForm {
+}
---
+++ boxfish-online-card-comment-manage/src/main/java/com/boxfishedu/card/comment/manage/service/CommentCardManageSdk/CommentCardManageSDK.java
@@ -0,0 +1,8 @@
+package com.boxfishedu.card.comment.manage.service.commentCardManageSDK;
+
+/**
+ * Created by ansel on 16/9/2.
+ */
+public class CommentCardManageSDK {
+
+}
