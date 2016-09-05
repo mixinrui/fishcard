d2d5a2c3b66006c799bd3dc847e5343d08cdd65c
ansel <liwanxiang@boxfish.cn>
2016-09-05 15:57:16 +0800

    no message

--- boxfish-online-card-comment-manage/src/main/java/com/boxfishedu/card/comment/manage/service/ForeignTeacherServiceImpl.java
+++ boxfish-online-card-comment-manage/src/main/java/com/boxfishedu/card/comment/manage/service/ForeignTeacherServiceImpl.java
@@ -5,12 +5,16 @@ import com.boxfishedu.card.comment.manage.entity.form.FromTeacherStudentForm;
 import com.boxfishedu.card.comment.manage.entity.form.TeacherForm;
 import com.boxfishedu.card.comment.manage.entity.jpa.EntityQuery;
 import com.boxfishedu.card.comment.manage.service.sdk.CommentCardManageSDK;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by ansel on 16/9/2.
@@ -48,7 +52,11 @@ public class ForeignTeacherServiceImpl implements ForeignTeacherService{
         EntityQuery entityQuery = new EntityQuery(entityManager,pageable) {
             @Override
             public Predicate[] predicates() {
-                
+                List<Predicate> predicateList = new ArrayList<>();
+                if (Objects.nonNull(teacherForm.getTeacherId())){
+                    predicateList.add(criteriaBuilder.equal(root.get("teacherId"),teacherForm.getTeacherId()));
+                }
+                if (StringUtils.isNotEmpty(teacherForm.getTeacherName()))
                 return new Predicate[0];
             }
         };
