eff248ee77ef506e1cfd6aae0d78eb00437e77bd
luolibing <397911353@qq.com>
2016-09-08 16:28:09 +0800

    修改课表为6个月

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/teacherrelated/TeacherAppRelatedServiceX.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/teacherrelated/TeacherAppRelatedServiceX.java
@@ -148,8 +148,8 @@ public class TeacherAppRelatedServiceX {
         DateRangeForm dateRangeForm;
         // 如果没有指定查询的年月,则默认查询半年的数据
         if (yearMonth == null) {
-            // 默认为当月+5个月,传参数默认为单月+1月
-            int months = count == null ? 5 : 1;
+            // 默认为当月+6个月,传参数默认为单月+1月
+            int months = count == null ? 6 : 1;
             dateRangeForm = DateUtil.createHalfYearDateRangeForm(count);
         } else {
             dateRangeForm = new DateRangeForm(
