a6bec50e6b7114b6e14dae2eaf832e7ae642c8ce
luolibing <397911353@qq.com>
2016-09-08 15:44:26 +0800

    兼容老版本

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/common/util/DateUtil.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/common/util/DateUtil.java
@@ -266,11 +266,10 @@ public class DateUtil {
      * 返回当前起2个月的区间
      * @return
      */
-    public static DateRangeForm createHalfYearDateRangeForm() {
+    public static DateRangeForm createHalfYearDateRangeForm(int months) {
         YearMonth now = YearMonth.now();
         Date from = convertToDate(now.atDay(1));
-        // FIXME 将半年数据改为2个月数据
-        Date to = convertToDate(now.plusMonths(1).atEndOfMonth());
+        Date to = convertToDate(now.plusMonths(months).atEndOfMonth());
         return new DateRangeForm(from, to);
     }
 
