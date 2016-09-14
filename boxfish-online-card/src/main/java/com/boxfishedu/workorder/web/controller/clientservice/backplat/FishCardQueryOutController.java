aa472cb066c5761ecb9854fc54372aa4dbc173e2
jiaozijun <java_jzj0828@163.com>
2016-09-14 12:53:33 +0800

    电话客服后台支持  新增

---
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/controller/clientservice/backplat/FishCardQueryOutController.java
@@ -0,0 +1,42 @@
+package com.boxfishedu.workorder.web.controller.clientservice.backplat;
+
+import com.boxfishedu.card.bean.CourseTypeEnum;
+import com.boxfishedu.mall.enums.OrderChannelDesc;
+import com.boxfishedu.workorder.common.bean.FishCardChargebackStatusEnum;
+import com.boxfishedu.workorder.entity.mongo.WorkOrderLog;
+import com.boxfishedu.workorder.service.workorderlog.WorkOrderLogService;
+import com.boxfishedu.workorder.servicex.fishcardcenter.FishCardQueryServiceX;
+import com.boxfishedu.workorder.web.param.FishCardFilterParam;
+import com.boxfishedu.workorder.web.view.base.JsonResultModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 电话客服后台支持
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/clientservice/backplat")
+public class FishCardQueryOutController {
+    @Autowired
+    private FishCardQueryServiceX fishCardQueryServiceX;
+
+    private Logger logger= LoggerFactory.getLogger(this.getClass());
+
+
+    /**
+     * 用户id不做限制的查询
+     */
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public JsonResultModel listFishCardsByCond(FishCardFilterParam fishCardFilterParam, Pageable pageable) {
+        return fishCardQueryServiceX.listFishCardsByUnlimitedUserCond(fishCardFilterParam,pageable);
+    }
+
+
+
+}
