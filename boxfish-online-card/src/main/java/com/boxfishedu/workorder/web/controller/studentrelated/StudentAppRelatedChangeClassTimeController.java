9d8c686486ec10250aa4c4c10007a9ba3e915be7
jiaozijun <java_jzj0828@163.com>
2016-09-16 19:40:12 +0800

    app换时间保存换时间

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/controller/studentrelated/StudentAppRelatedChangeClassTimeController.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/controller/studentrelated/StudentAppRelatedChangeClassTimeController.java
@@ -2,8 +2,10 @@ package com.boxfishedu.workorder.web.controller.studentrelated;
 
 import com.boxfishedu.workorder.common.util.DateUtil;
 import com.boxfishedu.workorder.servicex.CommonServeServiceX;
+import com.boxfishedu.workorder.servicex.fishcardcenter.FishCardModifyServiceX;
 import com.boxfishedu.workorder.servicex.studentrelated.*;
 import com.boxfishedu.workorder.web.param.AvaliableTimeParam;
+import com.boxfishedu.workorder.web.param.StartTimeParam;
 import com.boxfishedu.workorder.web.param.TimeSlotParam;
 import com.boxfishedu.workorder.web.view.base.JsonResultModel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +23,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/service/student/changeTime")
 public class
 StudentAppRelatedChangeClassTimeController {
+
     @Autowired
     private CommonServeServiceX commonServeServiceX;
+
     @Autowired
     private AvaliableTimeServiceXV1 avaliableTimeServiceXV1;
 
     @Autowired
     private AvaliableTimeForChangeTimeServiceXV avaliableTimeForChangeTimeServiceXV;
+
+    @Autowired
+    private FishCardModifyServiceX fishCardModifyServiceX;
 
 
     /**
@@ -41,6 +48,17 @@ StudentAppRelatedChangeClassTimeController {
     public JsonResultModel timeAvailableV1(Long workOrderId, Long userId) throws CloneNotSupportedException {
         commonServeServiceX.checkToken(userId,userId);
         return avaliableTimeForChangeTimeServiceXV.getTimeAvailable(workOrderId);
+    }
+
+    /**
+     * 更换上课时间
+     *
+     * @param startTimeParam 包含鱼卡id  开始时间
+     * @return
+     */
+    @RequestMapping(value = "/changeTime", method = RequestMethod.POST)
+    public JsonResultModel changeStartTime(@RequestBody StartTimeParam startTimeParam) {
+        return fishCardModifyServiceX.changeStartTime(startTimeParam);
     }
 
 
