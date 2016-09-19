f16337dedbebc743c7216d28584c765030057802
hucl <hucl@hucldeiMac.local>
2016-09-19 16:39:35 +0800

    提交鱼卡数据访问层类

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/dao/jpa/WorkOrderJpaRepository.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/dao/jpa/WorkOrderJpaRepository.java
@@ -66,7 +66,7 @@ public interface WorkOrderJpaRepository extends JpaRepository<WorkOrder, Long> {
 
     public List<WorkOrder> findByStatusInAndOrderIdLessThanAndEndTimeBetween(Integer[] statuses, long orderId, Date startDate, Date endDate);
 
-    public List<WorkOrder> findByIsCourseOverAndStatusInAndOrderIdLessThanAndEndTimeBetween(Short isCourseOver,Integer[] statuses, long orderId, Date startDate, Date endDate);
+    public List<WorkOrder> findByIsCourseOverAndStatusInAndOrderIdLessThanAndEndTimeBetween(Short isCourseOver, Integer[] statuses, long orderId, Date startDate, Date endDate);
 
     //按照订单id查找鱼卡
     public List<WorkOrder> findByOrderId(Long orderId);
@@ -78,59 +78,64 @@ public interface WorkOrderJpaRepository extends JpaRepository<WorkOrder, Long> {
     @Query("select wo from WorkOrder wo where wo.status =?1 and wo.service.amount>0")
     public List<WorkOrder> findWorkOrderContainBackOrder(int status);
 
-    public List<WorkOrder> findByStudentIdAndOrderIdAndStatusLessThan(Long studentId,Long orderId,Integer status);
+    public List<WorkOrder> findByStudentIdAndOrderIdAndStatusLessThan(Long studentId, Long orderId, Integer status);
 
-    public List<WorkOrder> findByStudentIdAndStatusLessThan(Long studentId,Integer status);
+    public List<WorkOrder> findByStudentIdAndStatusLessThan(Long studentId, Integer status);
 
 
-
-    /** begin 抢单接口 **/
+    /**
+     * begin 抢单接口
+     **/
 
     //  获取未来两天内 未安排教师的鱼卡信息  teacherid =0
-    public List<WorkOrder> findByTeacherIdAndStartTimeBetweenAndCreateTimeLessThanOrderByStartTime(Long teacherId, Date startDate, Date endDate,Date createTime);
+    public List<WorkOrder> findByTeacherIdAndStartTimeBetweenAndCreateTimeLessThanOrderByStartTime(Long teacherId, Date startDate, Date endDate, Date createTime);
 
     //  获取未来两天内 未安排教师的鱼卡信息 teacherid >0
     public List<WorkOrder> findByTeacherIdGreaterThanAndStartTimeBetween(Long teacherId, Date startDate, Date endDate);
 
-    /** end 抢单接口 **/
+    /**
+     * end 抢单接口
+     **/
 
 
     // 获取当前时间往前一整天(24小时)有换课纪录的鱼卡 状态30 updatetime不为空 teacherid>0    sendFlagCC  1 未发送
     //public List<WorkOrder> findByTeacherIdGreaterThanAndStatusAndUpdatetimeChangecourseBetween(Long teacherId,Integer status,Date begin ,Date end);
-    public List<WorkOrder> findByTeacherIdGreaterThanAndStatusAndSendflagccAndUpdatetimeChangecourseNotNullAndStartTimeBetween(Long teacherId,Integer status,String sendFlagCC,Date begin ,Date end);
+    public List<WorkOrder> findByTeacherIdGreaterThanAndStatusAndSendflagccAndUpdatetimeChangecourseNotNullAndStartTimeBetween(Long teacherId, Integer status, String sendFlagCC, Date begin, Date end);
 
 
     // 抢单之后,给课程匹配相应的老师
     @Modifying
     @Query("update WorkOrder o set o.teacherName= ?1 ,o.teacherId = ?2 ,o.status = ?3 ,updateTime=current_timestamp ,assignTeacherTime=current_timestamp   where o.id = ?4 and o.teacherId = ?5")
-    int setFixedTeacherNameAndTeacherIdAndStatusFor(String teacherName,Long teacherId ,Integer status, Long workorderId , Long teacherIdZero );
+    int setFixedTeacherNameAndTeacherIdAndStatusFor(String teacherName, Long teacherId, Integer status, Long workorderId, Long teacherIdZero);
 
 
     @Query("select wo from WorkOrder wo where wo.id in (?1) ")
-    public List<WorkOrder> findWorkOrderAll(Long [] ids);
-
+    public List<WorkOrder> findWorkOrderAll(Long[] ids);
 
 
     public List<WorkOrder> findByCourseType(String courseType);
 
-    public List<WorkOrder> findByStudentIdAndStatusLessThanAndStartTimeAfter(Long studentId,Integer status,Date beginDate);
+    public List<WorkOrder> findByStudentIdAndStatusLessThanAndStartTimeAfter(Long studentId, Integer status, Date beginDate);
 
 
     @Modifying
     @Query("update WorkOrder o set o.updatetimeRecharge= current_timestamp ,o.statusRecharge= ?1 where o.id = ?2 and o.statusRecharge=?3")
-    int setFixedStatusRechargeFor(Integer rechargeCodeAfter,Long id,Integer rechargeCodeBefore);
+    int setFixedStatusRechargeFor(Integer rechargeCodeAfter, Long id, Integer rechargeCodeBefore);
 
     //  获取未来两天内 未安排教师的鱼卡信息  teacherid =0
-    public List<WorkOrder> findByTeacherIdAndIsFreezeAndStartTimeBetweenAndCreateTimeLessThanOrderByStartTime(Long teacherId,Integer isFreeze, Date startDate, Date endDate,Date createTime);
+    public List<WorkOrder> findByTeacherIdAndIsFreezeAndStartTimeBetweenAndCreateTimeLessThanOrderByStartTime(Long teacherId, Integer isFreeze, Date startDate, Date endDate, Date createTime);
 
     //查找出学生所有状态的工单
     @Query("select distinct wo.studentId from WorkOrder wo where wo.orderId<11111111")
     public List<Long> findDistinctUsersFromWorkOrder();
 
-    public List<WorkOrder> findByStudentIdAndEndTimeLessThanOrderByStartTimeDesc(Long studentId,Date date);
+    public List<WorkOrder> findByStudentIdAndEndTimeLessThanOrderByStartTimeDesc(Long studentId, Date date);
 
-    public List<WorkOrder> findByStudentIdAndOrderChannelAndStartTimeAfter(Long studentId,String orderChannel,Date date);
+    public List<WorkOrder> findByStudentIdAndOrderChannelAndStartTimeAfter(Long studentId, String orderChannel, Date date);
 
-    /** 查询旷课的、还未扣积分的学生**/
+    /**
+     * 查询旷课的、还未扣积分的学生
+     **/
     @Query("select  wo from WorkOrder wo where wo.status = 51 and wo.deductScoreStatus is null and (wo.startTime between?1 and?2) and wo.orderChannel=?3")
     List<WorkOrder> queryAbsentStudent(Date startTime, Date endTime, String param);
+}
