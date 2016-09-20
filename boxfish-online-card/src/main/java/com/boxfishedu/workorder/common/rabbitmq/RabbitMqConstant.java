b8120e0c86acf45bd8b3f9caff9221935414d087
jiaozijun <java_jzj0828@163.com>
2016-09-20 18:40:53 +0800

    v1.0

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/common/rabbitmq/RabbitMqConstant.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/common/rabbitmq/RabbitMqConstant.java
@@ -88,4 +88,9 @@ public class RabbitMqConstant {
     //退款队列名称
     public static final String RECHARGE_WORKORDER_QUEUE= "boxfish.fishcard.recharge.queue";
 
+    /** 短信que end **/
+    public static final String SHORT_MESSAGE_TEMPLATE_NAME = "boxfish.short.message.template";
+    public static final String SHORT_MESSAGE_REPLY_TEMPLATE_NAME = "boxfish.short.message.reply.template";
+    /** 短信que end **/
+
 }
---
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/common/util/ShortMessageCodeConstant.java
@@ -0,0 +1,14 @@
+package com.boxfishedu.workorder.common.util;
+
+/**
+ * 发短信编码
+ * Created by jiaozijun on 16/9/20.
+ */
+public class ShortMessageCodeConstant {
+
+    /** 学生明天有课提醒 编码 **/
+    public final  static String SMS_STU_NOTITY_TOMO_CODE="SMS_15120322";
+
+    /** 老师课程取消短信通知编码 **/
+    public final  static String SMS_TEA_NOTITY_CLASS_CANCEL_CODE="SMS_15115267";
+}
