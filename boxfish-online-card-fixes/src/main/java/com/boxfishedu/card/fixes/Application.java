0208e3e67f80be83918c2b8ebeeb91e0883b1bf3
luolibing <397911353@qq.com>
2016-10-22 15:40:09 +0800

    英文名问题

--- boxfish-online-card-fixes/src/main/java/com/boxfishedu/card/fixes/Application.java
+++ boxfish-online-card-fixes/src/main/java/com/boxfishedu/card/fixes/Application.java
@@ -1,6 +1,7 @@
 package com.boxfishedu.card.fixes;
 
 import com.boxfishedu.card.fixes.entity.mongo.ScheduleCourseInfoMorphiaRepository;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -21,6 +22,15 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        for(int i = 0, size = args.length; i < size; i++) {
+            if(StringUtils.startsWith(args[i], "single=")) {
+                String[] params = args[i].split("=");
+                if(params.length ==2 ) {
+                    repository.updateCourseInfo(Long.valueOf(params[1]));
+                    return;
+                }
+            }
+        }
         repository.updateCourseInfos();
         System.out.println("finish fixes");
     }
--- boxfish-online-card-fixes/src/main/java/com/boxfishedu/card/fixes/entity/mongo/ScheduleCourseInfoMorphiaRepository.java
+++ boxfish-online-card-fixes/src/main/java/com/boxfishedu/card/fixes/entity/mongo/ScheduleCourseInfoMorphiaRepository.java
@@ -12,29 +12,39 @@ import java.util.Objects;
 @Component
 public class ScheduleCourseInfoMorphiaRepository extends BaseMorphiaRepository<ScheduleCourseInfo> {
 
+    private final static String url = "http://base.boxfish.cn/course/info/%s";
+
     public void updateCourseInfos() {
-        String url = "http://base.boxfish.cn/course/info/%s";
+
         Query<ScheduleCourseInfo> query = datastore.createQuery(ScheduleCourseInfo.class);
         List<ScheduleCourseInfo> list = query.asList();
         System.out.println(list.size());
+        list.parallelStream().forEach(this::updateCourseInfo);
+    }
 
-        list.parallelStream().forEach( sci -> {
-            System.out.println("before update= [{" + sci + "}]");
-            if(StringUtils.isNotEmpty(sci.getCourseId())) {
-                try {
-                    Map courseMap = new RestTemplate().getForObject(
-                            String.format(url, sci.getCourseId()), Map.class);
-                    if(!Objects.isNull(courseMap) && !Objects.isNull(courseMap.get("englishName"))) {
-                        sci.setEnglishName((String) courseMap.get("englishName"));
-                    } else {
-                        sci.setEnglishName("");
-                    }
-                    datastore.save(sci);
-                    System.out.println("after  update= [{" + sci + "}]");
-                } catch (Exception e) {
-//                    e.printStackTrace();
+    public void updateCourseInfo(Long id) {
+        Query<ScheduleCourseInfo> query = datastore.createQuery(ScheduleCourseInfo.class);
+        query.criteria("workOrderId").equal(id);
+        ScheduleCourseInfo courseInfo = query.get();
+        updateCourseInfo(courseInfo);
+    }
+
+    private void updateCourseInfo(ScheduleCourseInfo sci) {
+        System.out.println("before update= [{" + sci + "}]");
+        if(StringUtils.isNotEmpty(sci.getCourseId())) {
+            try {
+                Map courseMap = new RestTemplate().getForObject(
+                        String.format(url, sci.getCourseId()), Map.class);
+                if(!Objects.isNull(courseMap) && !Objects.isNull(courseMap.get("englishName"))) {
+                    sci.setEnglishName((String) courseMap.get("englishName"));
+                } else {
+                    sci.setEnglishName("");
                 }
+                datastore.save(sci);
+                System.out.println("after  update= [{" + sci + "}]");
+            } catch (Exception e) {
+//                    e.printStackTrace();
             }
-        });
+        }
     }
 }
