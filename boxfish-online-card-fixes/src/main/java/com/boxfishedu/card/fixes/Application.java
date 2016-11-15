74a8b69643de8bec7db2dcf6e9ba7d4e8d74161d
luolibing <luolibing@boxfish.cn>
2016-11-15 10:44:47 +0800

    添加接口

--- boxfish-online-card-fixes/src/main/java/com/boxfishedu/card/fixes/Application.java
+++ boxfish-online-card-fixes/src/main/java/com/boxfishedu/card/fixes/Application.java
@@ -3,7 +3,6 @@ package com.boxfishedu.card.fixes;
 import com.boxfishedu.card.fixes.entity.mongo.ScheduleCourseInfoMorphiaRepository;
 import com.boxfishedu.card.fixes.service.WorkOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
 
 
     public static void main(String[] args) {
@@ -29,34 +28,20 @@ public class Application implements CommandLineRunner {
     @Autowired
     private WorkOrderService workOrderService;
 
-    @Override
-    public void run(String... args) throws Exception {
-//        System.out.println("参数" + Arrays.toString(args));
-//        String type;
-//        // args.length == 0
-//        if(true) {
-//            type = "4";
-//        } else {
-//            type = args[0];
-//        }
-//        try {
-//            switch (type) {
-//                case "0": repository.updateCourseInfos(); break;
-//                case "1": repository.updateCourseDifficultys(); break;
-//                case "2": workOrderService.handleAllDifferent(); break;
-//                case "3": workOrderService.handleScheduleCourseInfo(); break;
-//                case "4": repository.updateCourseEnglishNames(); break;
-//            }
-//            System.out.println("finish fixes");
-//            Runtime.getRuntime().exit(0);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            Runtime.getRuntime().exit(1);
-//        }
-    }
-
     @RequestMapping(value = "/courseInfo/synchronous/{courseId}", method = RequestMethod.GET)
     public void courseInfoSynchronous(@PathVariable String courseId) {
         workOrderService.synchronousCourseInfoById(courseId);
+    }
+
+    @RequestMapping(value = "/fixes/{type}", method = RequestMethod.PUT)
+    public void fixes(@PathVariable Integer type) {
+        switch (type) {
+            case 0: repository.updateCourseInfos(); break;
+            case 1: repository.updateCourseDifficultys(); break;
+            case 2: workOrderService.handleAllDifferent(); break;
+            case 3: workOrderService.handleScheduleCourseInfo(); break;
+            case 4: repository.updateCourseEnglishNames(); break;
+        }
+        System.out.println("finish fixes");
     }
 }
