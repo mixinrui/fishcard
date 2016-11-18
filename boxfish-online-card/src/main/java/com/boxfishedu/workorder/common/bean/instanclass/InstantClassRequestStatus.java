add4188f119f3283755f3e65575e2421f1babfe3
hucl <hucl@hucldeiMac.local>
2016-11-18 17:54:31 +0800

    修改提示

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/common/bean/instanclass/InstantClassRequestStatus.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/common/bean/instanclass/InstantClassRequestStatus.java
@@ -2,7 +2,7 @@ package com.boxfishedu.workorder.common.bean.instanclass;
 
 public enum InstantClassRequestStatus {
     UNKNOWN(0,"默认状态"),
-    WAIT_TO_MATCH(10,"等待匹配教师"),
+    WAIT_TO_MATCH(10,"正在呼叫外教..."),
     ASK_TOO_BUSY(20,"您访问过于频繁"),
     HAVE_CLASS_IN_HALF_HOURS(30,"半小时内有即将上的课程,不能立即上课"),
     NOT_IN_RANGE(31,"该时间段不允许即时上课"),
@@ -10,7 +10,7 @@ public enum InstantClassRequestStatus {
     OUT_OF_NUM(33,"先去购买外教课程吧,然后才能实时上课~"),
     FAIL_RECOMMAND_COURSE(34,"暂时没有与你Level匹配的课程,请调整起点Level或者稍后再试吧"),
     NO_MATCH(40,"现在是上课高峰,稍后再来试试吧"),
-    MATCHED(50,"你发起的立即上课,已有外教回应接受~稍作等待,外教马上会给你发起邀请");
+    MATCHED(50,"老师即将开始上课...");
 
     private int code;
     private String desc;
