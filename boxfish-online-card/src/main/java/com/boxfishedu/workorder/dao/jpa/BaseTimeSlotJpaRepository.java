7cad154772ec0a901d41241037c107328ef5887c
jiaozijun <java_jzj0828@163.com>
2016-11-26 17:41:00 +0800

    查询接口

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/dao/jpa/BaseTimeSlotJpaRepository.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/dao/jpa/BaseTimeSlotJpaRepository.java
@@ -3,6 +3,8 @@ package com.boxfishedu.workorder.dao.jpa;
 import com.boxfishedu.workorder.common.redis.CacheKeyConstant;
 import com.boxfishedu.workorder.entity.mysql.BaseTimeSlots;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -19,4 +21,8 @@ public interface BaseTimeSlotJpaRepository extends JpaRepository<BaseTimeSlots, 
 
     @Query(value = "select max(e.classDate) from BaseTimeSlots e")
     Date findMaxDate();
+
+    public Page<BaseTimeSlots> findByTeachingTypeAndClassDateBetween(Integer teachingType, Date beginDate, Date endDate, Pageable pageable);
+
+    public List<BaseTimeSlots> findByTeachingTypeAndClassDateBetween(Integer teachingType, Date beginDate, Date endDate);
 }
