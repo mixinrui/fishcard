4b0122f14f7b15059392fb881925cc23b5d0eba4
jiaozijun <java_jzj0828@163.com>
2016-12-03 14:24:44 +0800

    给美林提供的接口服务，不影响其他业务，查询 功能。

---
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/param/FishCardinnerParam.java
@@ -0,0 +1,14 @@
+
+package com.boxfishedu.workorder.web.param;
+
+import lombok.Data;
+
+/**
+ * Created by jiaozijun on 16/12/3.
+ */
+@Data
+public class FishCardinnerParam {
+
+    private  Long studentId;
+    private  Long[] hasEvFishcardIds;  // 鱼卡数组
+}
