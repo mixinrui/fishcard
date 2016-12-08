3608b4184139539ccecd1a57accd825ea2261102
jiaozijun <java_jzj0828@163.com>
2016-12-08 23:00:18 +0800

    完成MD5加密验证

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/fishcardcenter/FishCardQueryServiceX.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/fishcardcenter/FishCardQueryServiceX.java
@@ -23,12 +23,9 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
-import org.springframework.util.StringUtils;
-import sun.misc.BASE64Encoder;
 
-import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -113,18 +110,18 @@ public class FishCardQueryServiceX {
 
 
     public String EncoderByMd5(String str) {
-        String newstr = "";
         try {
-            //确定计算方法
-            MessageDigest md5 = MessageDigest.getInstance("MD5");
-            BASE64Encoder base64en = new BASE64Encoder();
-            //加密后的字符串
-            newstr = base64en.encode(md5.digest(str.getBytes()));
+            // 生成一个MD5加密计算摘要
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            // 计算md5函数
+            md.update(str.getBytes());
+            // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
+            // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
+            return new BigInteger(1, md.digest()).toString(16);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        return newstr;
+        return "";
     }
 
 
