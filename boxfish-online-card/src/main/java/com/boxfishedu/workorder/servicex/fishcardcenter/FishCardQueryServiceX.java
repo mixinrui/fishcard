85623722fc646a41748bac1a5da28e4226fbe4f6
jiaozijun <java_jzj0828@163.com>
2016-12-09 16:21:36 +0800

    更改加密方法

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/fishcardcenter/FishCardQueryServiceX.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/fishcardcenter/FishCardQueryServiceX.java
@@ -15,6 +15,7 @@ import com.boxfishedu.workorder.web.param.FishCardFilterParam;
 import com.boxfishedu.workorder.web.view.fishcard.FishCardGroupsInfo;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -113,13 +114,7 @@ public class FishCardQueryServiceX {
 
     public String EncoderByMd5(String str) {
         try {
-            // 生成一个MD5加密计算摘要
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            // 计算md5函数
-            md.update(str.getBytes());
-            // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
-            // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
-            return new BigInteger(1, md.digest()).toString(16);
+            return   DigestUtils.md5Hex(str.getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }
