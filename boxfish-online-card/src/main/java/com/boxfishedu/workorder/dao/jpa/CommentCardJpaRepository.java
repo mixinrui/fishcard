13e97fc1a724a2d558766810f8ce04c16b0d457b
ansel <liwanxiang@boxfish.cn>
2016-12-12 14:37:30 +0800

    no message

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/dao/jpa/CommentCardJpaRepository.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/dao/jpa/CommentCardJpaRepository.java
@@ -106,7 +106,7 @@ public interface CommentCardJpaRepository extends JpaRepository<CommentCard, Lon
     /**
      * 初始获取学生首页外教点评
      */
-    @Query("select c from CommentCard c where c.studentId =?1 and c.status in (400,600) and  c.updateTime = " +
+    @Query("select c from CommentCard c where c.studentId =?1 and c.status in (400,600) and  c.teacherAnswerTime = " +
             "(select max(cd.teacherAnswerTime) from CommentCard cd where cd.studentId =?1 and cd.status in (400,600))")
     public CommentCard getHomePageCommentCard(Long studentId);
 
