110e1bdf493dc1fdc93fab7c5c5673fc26858e25
jiaozijun <java_jzj0828@163.com>
2016-12-17 18:10:51 +0800

    按钮是否出现

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/dao/jpa/StStudentSchemaJpaRepository.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/dao/jpa/StStudentSchemaJpaRepository.java
@@ -9,4 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface StStudentSchemaJpaRepository extends JpaRepository<StStudentSchema, Long> {
     StStudentSchema findByStudentId(Long studentId);
     StStudentSchema findByStudentIdAndTeacherIdAndStSchema(Long studentId,Long teacherId, StStudentSchema.StSchema stSchema);
+
+    // 查询schema  学生id  st_schema  sku_id
+    public StStudentSchema findTop1ByStudentIdAndStSchemaAndSkuId(Long studentId,StStudentSchema.StSchema stSchema,StStudentSchema.CourseType courseType);
 }
