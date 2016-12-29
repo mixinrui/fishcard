bc70981cf51b67aeabe806f89bb05c6d28af57d9
jiaozijun <java_jzj0828@163.com>
2016-12-30 01:13:07 +0800

    服务器控制防治重复提交问题。

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/common/redis/CacheKeyConstant.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/common/redis/CacheKeyConstant.java
@@ -18,6 +18,8 @@ public class CacheKeyConstant {
 
     public static final String WORKORDERS_REPEATED_SUBMISSION = "WORKORDERS_REPEATED_SUBMISSION";
 
+    public static final String ASSIGN_REPEATED_SUBMISSION = "ASSIGN_REPEATED_SUBMISSION";
+
     public static final String WORKORDERS_INSTANT_CLASS = "INSTANT_CLASS";
 
     public static final String BASE_TIME_SLOTS = "BASE_TIME_SLOTS";
