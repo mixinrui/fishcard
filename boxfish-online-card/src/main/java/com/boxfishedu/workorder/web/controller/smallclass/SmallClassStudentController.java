9ea0baadd81a677116eff4c45f1c69e0dc67cdad
hucl <hucl@hucldeiMac.local>
2017-01-09 18:46:20 +0800

    提交小班课接口

---
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/controller/smallclass/SmallClassStudentController.java
@@ -0,0 +1,14 @@
+package com.boxfishedu.workorder.web.controller.smallclass;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by hucl on 17/1/9.
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/service/student/smallclass")
+public class SmallClassStudentController {
+}
