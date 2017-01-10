ed4d301255e5956abb663613473a26c68da0f59c
luolibing <luolibing@boxfish.cn>
2017-01-10 17:44:44 +0800

    添加缓存

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/dao/jpa/SmallClassJpaRepository.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/dao/jpa/SmallClassJpaRepository.java
@@ -1,10 +1,10 @@
 package com.boxfishedu.workorder.dao.jpa;
 
 import com.boxfishedu.workorder.entity.mysql.SmallClass;
-import com.boxfishedu.workorder.entity.mysql.WorkOrder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Date;
 import java.util.List;
@@ -16,5 +16,6 @@ public interface SmallClassJpaRepository extends JpaRepository<SmallClass, Long>
 
     List<SmallClass> findByClassDateAndSlotIdAndSmallClassType(Date classDate, Integer slotId, String classType);
 
+    @Query("select s from SmallClass s order by s.classDate")
     Page<SmallClass> findPage(Pageable pageable);
 }
--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/studentrelated/PublicClassRoom.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/servicex/studentrelated/PublicClassRoom.java
@@ -9,8 +9,12 @@ import com.boxfishedu.workorder.entity.mysql.SmallClass;
 import com.boxfishedu.workorder.service.ServiceSDK;
 import com.boxfishedu.workorder.web.view.base.JsonResultModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.Map;
 import java.util.Optional;
@@ -19,19 +23,25 @@ import java.util.Optional;
  * Created by LuoLiBing on 17/1/9.
  * 公开课堂
  */
-@Component
+@Service
 public class PublicClassRoom {
 
-    private static PublicClassInfoJpaRepository publicClassInfoJpaRepository;
-
-    private static ServiceSDK serviceSdk;
+    private final static String CLASS_ROOM_MEMBER_KEY = "classRoom_member_";
 
     @Autowired
-    public void init(PublicClassInfoJpaRepository classInfoJpaRepository, ServiceSDK ss) {
-        publicClassInfoJpaRepository = classInfoJpaRepository;
-        serviceSdk = ss;
+    private PublicClassInfoJpaRepository publicClassInfoJpaRepository;
+
+    @Autowired
+    private ServiceSDK serviceSdk;
+
+    private SetOperations<String, Long> setOperations;
+
+    @Autowired
+    public void initRedis(@Qualifier(value = "stringLongRedisTemplate") RedisTemplate<String, Long> redisTemplate) {
+        setOperations = redisTemplate.opsForSet();
     }
 
+    @Transactional
     public void enter(SmallClass smallClass, Long studentId, String accessToken) {
         // 是否是在正常的时间点
 
@@ -45,6 +55,9 @@ public class PublicClassRoom {
 
         // 三 如果没有消费过, 则直接进入房间, 保存
         savePublicClassInfo(smallClass, studentId);
+
+        // 四 更新redis
+        updateCache(smallClass, studentId);
     }
 
     private void savePublicClassInfo(SmallClass smallClass, Long studentId) {
@@ -56,10 +69,21 @@ public class PublicClassRoom {
         publicClassInfoJpaRepository.save(entity);
     }
 
+    /**
+     * 判断是否进入过房间
+     * @param smallClassId
+     * @param studentId
+     * @return
+     */
     private boolean isOnceEntered(Long smallClassId, Long studentId) {
-        Optional<PublicClassInfo> pciOptional = publicClassInfoJpaRepository.findBySmallClassIdAndStudentId(
-                smallClassId, studentId);
-        return (pciOptional.isPresent());
+        try {
+            // 优先使用缓存, 只有在缓存出错的情况下才使用数据库查询
+            return setOperations.isMember(CLASS_ROOM_MEMBER_KEY + smallClassId, studentId);
+        } catch (Exception e) {
+            Optional<PublicClassInfo> pciOptional = publicClassInfoJpaRepository.findBySmallClassIdAndStudentId(
+                    smallClassId, studentId);
+            return (pciOptional.isPresent());
+        }
     }
 
     private void checkMember(SmallClass smallClass, Long studentId, String accessToken) {
@@ -108,4 +132,16 @@ public class PublicClassRoom {
             throw new PublicClassException(PublicClassMessageEnum.EVERY_DAY_LIMIT);
         }
     }
+
+    /**
+     * 更新缓存
+     * @param classRoom
+     * @param studentId
+     */
+    private void updateCache(SmallClass classRoom, Long studentId) {
+        // 将学生加入到这个课堂当中,下次直接判断是否在缓存中
+        setOperations.add(CLASS_ROOM_MEMBER_KEY + classRoom.getId(), studentId);
+    }
+
+
 }
