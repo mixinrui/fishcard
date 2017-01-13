f08884933ea30f7a390a6d2dff83309f082775a5
hucl <hucl@hucldeiMac.local>
2017-01-13 12:14:50 +0800

    提交教师校验接口

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/controller/smallclass/SmallClassTeacherController.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/controller/smallclass/SmallClassTeacherController.java
@@ -1,9 +1,11 @@
 package com.boxfishedu.workorder.web.controller.smallclass;
 
 import com.boxfishedu.workorder.common.bean.instanclass.ClassTypeEnum;
+import com.boxfishedu.workorder.dao.jpa.SmallClassJpaRepository;
 import com.boxfishedu.workorder.entity.mysql.SmallClass;
 import com.boxfishedu.workorder.web.view.base.JsonResultModel;
 import com.google.common.collect.Maps;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
@@ -16,31 +18,17 @@ import java.util.Map;
 @RestController
 @RequestMapping("/service/teacher/smallclass")
 public class SmallClassTeacherController {
+
+    @Autowired
+    private SmallClassJpaRepository smallClassJpaRepository;
+
     @RequestMapping(value = "/{smallclass_id}/validate", method = RequestMethod.GET)
     public JsonResultModel validate(@PathVariable("smallclass_id") Long smallClassId) {
         Map<String, Object> map = Maps.newLinkedHashMap();
         //10:too early   20:completed   30:success
         map.put("status", 30);
         map.put("statusDesc", "success");
-
-        SmallClass smallClass = new SmallClass();
-        smallClass.setGroupLeaderCard(1L);
-        smallClass.setGroupLeader(11L);
-        smallClass.setClassType(ClassTypeEnum.SMALL.name());
-        smallClass.setRoleId(2);
-        smallClass.setChatRoomId(111111L);
-        smallClass.setClassDate(new Date());
-        smallClass.setCourseId("JJJKKKKKLLLSS");
-        smallClass.setCourseName("test course");
-        smallClass.setCourseType("TALK");
-        smallClass.setCreateTime(new Date());
-        smallClass.setGroupId("group id");
-        smallClass.setGroupName("group name");
-        smallClass.setStatus(200);
-        smallClass.setSlotId(20);
-        smallClass.setTeacherId(222L);
-        smallClass.setUpdateTime(new Date());
-        smallClass.setId(333L);
+        SmallClass smallClass = smallClassJpaRepository.findOne(smallClassId);
 
         map.put("classInfo", smallClass);
         return JsonResultModel.newJsonResultModel(map);
@@ -48,24 +36,7 @@ public class SmallClassTeacherController {
 
     @RequestMapping(value = "/{smallclass_id}/detail", method = RequestMethod.GET)
     public JsonResultModel classDetail(@PathVariable("smallclass_id") Long smallClassId) {
-        SmallClass smallClass = new SmallClass();
-        smallClass.setGroupLeaderCard(1L);
-        smallClass.setGroupLeader(11L);
-        smallClass.setClassType(ClassTypeEnum.PUBLIC.name());
-        smallClass.setRoleId(2);
-        smallClass.setChatRoomId(111111L);
-        smallClass.setClassDate(new Date());
-        smallClass.setCourseId("JJJKKKKKLLLSS");
-        smallClass.setCourseName("test course");
-        smallClass.setCourseType("TALK");
-        smallClass.setCreateTime(new Date());
-        smallClass.setGroupId("group id");
-        smallClass.setGroupName("group name");
-        smallClass.setStatus(200);
-        smallClass.setSlotId(20);
-        smallClass.setTeacherId(222L);
-        smallClass.setUpdateTime(new Date());
-        smallClass.setId(333L);
+        SmallClass smallClass = smallClassJpaRepository.findOne(smallClassId);
         return JsonResultModel.newJsonResultModel(smallClass);
     }
 
