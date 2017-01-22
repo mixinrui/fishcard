357d0e1f25bb27eb3146620db96a9524cb9cc7b8
hucl <hucl@hucldeiMac.local>
2017-01-22 15:25:58 +0800

    增加文字描述

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/controller/smallclass/SmallClassStudentController.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/web/controller/smallclass/SmallClassStudentController.java
@@ -1,13 +1,11 @@
 package com.boxfishedu.workorder.web.controller.smallclass;
 
-import com.boxfishedu.workorder.servicex.instantclass.container.ThreadLocalUtil;
 import com.boxfishedu.workorder.servicex.smallclass.SmallClassStudentStatusServiceX;
 import com.boxfishedu.workorder.web.view.base.JsonResultModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * Created by hucl on 17/1/9.
@@ -32,6 +30,7 @@ public class SmallClassStudentController {
 
     @RequestMapping(value = "/publicTips", method = RequestMethod.GET)
     public JsonResultModel publicTips() {
+        //成为BOXFiSH学员 即可每天免费上课
         return JsonResultModel.newJsonResultModel("公开课预计于2月15号开放,欢迎使用");
     }
 
