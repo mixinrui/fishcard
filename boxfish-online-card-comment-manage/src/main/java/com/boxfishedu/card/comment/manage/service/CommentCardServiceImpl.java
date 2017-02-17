2aff517b47723bc3611227db949509178606dbaf
anselTyper <liwanxiang@boxfish.cn>
2017-02-17 14:55:32 +0800

    外教点评后台过滤条件修改

--- boxfish-online-card-comment-manage/src/main/java/com/boxfishedu/card/comment/manage/service/CommentCardServiceImpl.java
+++ boxfish-online-card-comment-manage/src/main/java/com/boxfishedu/card/comment/manage/service/CommentCardServiceImpl.java
@@ -241,7 +241,7 @@ public class CommentCardServiceImpl implements CommentCardService {
             }
             // 超时未点评
             else if(Objects.equals(commentCardForm.getStatus(), CommentCardFormStatus.TIMEOUT.value())) {
-                predicateList.add(criteriaBuilder.lt(
+                predicateList.add(criteriaBuilder.gt(
                         root.get("status"), CommentCardStatus.ANSWERED.getCode()
                 ));
                 NotAnswerTime.DateRange dateRange = NotAnswerTime._24_48HOURS.getRange();
