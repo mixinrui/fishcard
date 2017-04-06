c15cb35d96c39d76110dad06288559cd33d4afd5
jiaozijun <java_jzj0828@163.com>
2017-04-06 11:32:33 +0800

    小班课超级用户删除课程接口

--- boxfish-online-card/src/main/java/com/boxfishedu/workorder/service/monitor/MonitorUserService.java
+++ boxfish-online-card/src/main/java/com/boxfishedu/workorder/service/monitor/MonitorUserService.java
@@ -31,11 +31,6 @@ public class MonitorUserService {
     @Autowired
     MonitorUserCourseJpaRepository monitorUserCourseJpaRepository;
 
-    @Autowired
-    private WorkOrderJpaRepository workOrderJpaRepository;
-
-    @Autowired
-    private CourseScheduleRepository courseScheduleRepository;
 
     public List<MonitorUser> getAllSuperUser(){
         logger.info("@getAllSuperUser checking for login ...");
@@ -143,37 +138,5 @@ public class MonitorUserService {
     }
 
 
-    /**
-     * 超级用户更换课程需要清理之前的鱼卡课程
-     * @param studentId
-     * @param smallClassId
-     * @return
-     */
-    @Transactional
-    public boolean  deleteMoniorCourse(Long studentId,Long smallClassId){
-        if(Objects.isNull(studentId) || Objects.isNull(smallClassId)){
-            logger.info("@deleteMoniorCourse_params_not_available");
-            return false;
-        }
-        WorkOrder workOrder = workOrderJpaRepository.findBySmallClassIdAndStudentId(studentId,smallClassId);
-        if(Objects.isNull(workOrder)){
-            logger.info("@deleteMoniorCourse_params_no_class");
-            //还没有产生鱼卡 可以进行更换操作
-            return true;
-        }
 
-        CourseSchedule courseSchedule =  courseScheduleRepository.findByWorkorderId(workOrder.getId());
-        if(Objects.isNull(courseSchedule)){
-            //课程信息有误不能更换,请先核对数据在进行更换
-            logger.error("@deleteMoniorCourse_date_error");
-            return false;
-        }
-
-
-        workOrderJpaRepository.delete(workOrder.getId());
-        courseScheduleRepository.delete(courseSchedule.getId());
-
-        return true;
-
-     }
 }
