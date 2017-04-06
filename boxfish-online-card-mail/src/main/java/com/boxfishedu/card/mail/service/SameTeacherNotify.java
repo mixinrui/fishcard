4652d422b86e18480ffc07414866d40a842c90af
jiaozijun <java_jzj0828@163.com>
2017-04-06 09:28:51 +0800

    同一个老师在一个时间片有两节课

---
+++ boxfish-online-card-mail/src/main/java/com/boxfishedu/card/mail/service/SameTeacherNotify.java
@@ -0,0 +1,58 @@
+package com.boxfishedu.card.mail.service;
+
+import com.boxfishedu.card.mail.config.SameTeacherJavaMailConfig;
+import com.boxfishedu.card.mail.config.WorkOrderJavaMailConfig;
+import com.boxfishedu.card.mail.entity.jpa.WorkOrderJpaRepository;
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 相同老师同一个时间片 有两节课
+ * Created by jiaozijun on 17/04/05.
+ */
+@Service
+public class SameTeacherNotify {
+
+    private final static Logger logger = LoggerFactory.getLogger(SameTeacherNotify.class);
+
+    @Autowired
+    private WorkOrderJpaRepository workOrderJpaRepository;
+
+    @Autowired
+    @Qualifier(value = "sameTeacherTemplate")
+    private SimpleMailMessage templateMessage;
+
+    @Autowired
+    private SameTeacherJavaMailConfig javaMailConfig;
+
+    @Scheduled(cron = "0 0 4 * * *")
+    public void notifySameTeacher() throws MessagingException {
+        List<Map<String, Object>> workOrders = getSameTeacherList();
+        logger.info("notifySameTeacher workOrder, find out size=[{}]", workOrders == null ? 0 : workOrders.size());
+        if(CollectionUtils.isNotEmpty(workOrders)) {
+            new CardMimeMailSender()
+                    .createMimeMail(templateMessage)
+                    .setRecipients(javaMailConfig.getRecipients())
+                    .setMailContent("same_teacher_notify", Collections.singletonMap("workOrders", workOrders))
+                    .sendMail();
+        }
+    }
+
+    private List<Map<String, Object>> getSameTeacherList() {
+        return workOrderJpaRepository.getSameClassOfTeacher();
+    }
+}
